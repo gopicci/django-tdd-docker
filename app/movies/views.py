e3c258a828f416c2ea0814726150ae0a193dee88
gopicci <56777940+gopicci@users.noreply.github.com>
2020-05-19 13:56:07 -0700

    Defining remaining routes

--- app/movies/views.py
+++ app/movies/views.py
@@ -57,3 +57,18 @@ class MovieViewSet(ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        queryset = Movie.objects.all()
+        movie = get_object_or_404(queryset, pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk, format=None):
+        queryset = Movie.objects.all()
+        movie = get_object_or_404(queryset, pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -68,3 +68,67 @@ def test_get_all_movies(client, add_movie):
     assert resp.status_code == 200
     assert resp.data[0]["title"] == movie_one.title
     assert resp.data[1]["title"] == movie_two.title
+
+
+@pytest.mark.django_db
+def test_remove_movie(client, add_movie):
+    movie = add_movie(title="Ciuruma", genre="horror", year="2018")
+
+    resp = client.get(f"/api/movies/{movie.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "Ciuruma"
+
+    resp_two = client.delete(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 204
+
+    resp_three = client.get(f"/api/movies/")
+    assert resp.status_code == 200
+    assert len(resp_three.data) == 0
+
+
+@pytest.mark.django_db
+def test_remove_movie_incorrect_id(client):
+    resp = client.delete(f"/api/movie/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_movie(client, add_movie):
+    movie = add_movie(title="Ciuruma", genre="horror", year="2018")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "Ciuruma", "genre": "horror", "year": "2017"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 200
+    assert resp.data["title"] == "Ciuruma"
+    assert resp.data["year"] == "2017"
+
+    resp_two = client.get(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["title"] == "Ciuruma"
+    assert resp_two.data["year"] == "2017"
+
+
+@pytest.mark.django_db
+def test_update_movie_incorrect_id(client):
+    resp = client.put(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "add_movie, payload, status_code",
+    [
+        ["add_movie", {}, 400],
+        ["add_movie", {"title": "Ciuruma", "genre": "horror"}, 400],
+    ],
+    indirect=["add_movie"],
+)
+def test_update_movie_invalid_json(client, add_movie, payload, status_code):
+    movie = add_movie(title="Ciuruma", genre="horror", year="2018")
+    resp = client.put(
+        f"/api/movies/{movie.id}/", payload, content_type="application/json"
+    )
+    assert resp.status_code == status_code
